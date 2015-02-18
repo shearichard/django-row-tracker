61175c6f87884cda9f651cde85a0642b3d427ae9
Richard Shea <rshea@thecubagroup.com>
2015-02-18 15:26:21 +1300

    Now able to work from any directory and, via command line args, gets pointed at the project of interest

--- django-row-tracker.py
+++ django-row-tracker.py
@@ -1,20 +1,21 @@
 import argparse
 import sys
+import os
 
-from django.core.management import setup_environ
-
-import settings
-setup_environ(settings)
-
-from django.db.utils import DatabaseError
-
-from django.db.transaction import rollback_unless_managed
-from django.db import models
+#from django.core.management import setup_environ
+#
+#import settings
+#setup_environ(settings)
 
 def get_model_info():
     '''
     Dump all models and their row counts to the screen
     '''
+    from django.conf import settings
+    from django.db.utils import DatabaseError
+    from django.db.transaction import rollback_unless_managed
+    from django.db import models
+
     project_models = models.get_models(include_auto_created=True)
 
     for them in project_models:
@@ -31,17 +32,29 @@ def process_args():
     '''
     Process command line args
     '''
-    parser = argparse.ArgumentParser()
-    parser.add_argument("path_to_project_root")
-    parser.add_argument("project_name")
+    parser = argparse.ArgumentParser("Obtains and records the number of rows in a tables of a Django Project at run time")
+    parser.add_argument('--version', action='version', version='%(prog)s - Release : 1.0')
+    parser.add_argument("path_to_project_root", help="Path to the root of the Django project of interest (that is the directory below 'manage.py')")
+    parser.add_argument("project_name", help='''The name of the Django project of interest''')
     args = parser.parse_args()
+
     return args
+
+def setup_env(args):
+    '''
+    Do what's necessary to allow this script to access the
+    ORM of the Django project specified on the command line
+    '''
+    sys.path.append(os.path.abspath(args.path_to_project_root))
+    sys.path.append('%s/%s' % (os.path.abspath(args.path_to_project_root), args.project_name))
+    os.environ['DJANGO_SETTINGS_MODULE'] = '%s.settings' % args.project_name
 
 def main():
     '''
     Handle main processing
     '''
     args = process_args()
+    setup_env(args)
     get_model_info()
 
 
