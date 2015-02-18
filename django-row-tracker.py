b73b895076e3aca651baaef31324be1c30fad10e
Richard Shea <rshea@thecubagroup.com>
2015-02-18 23:11:06 +1300

    Allow to capture run description and to switch stdout on/off

--- django-row-tracker.py
+++ django-row-tracker.py
@@ -31,7 +31,8 @@ def create_tables_if_necessary(conn):
             [RUN_ID] INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,  \
             [RUN_PRO_ID] INTEGER  NULL,  \
             [RUN_TIME] TIMESTAMP DEFAULT CURRENT_TIMESTAMP NULL,  \
-            [RUN_COMMENT] NVARCHAR(256)  UNIQUE NULL  \
+            [RUN_LOCAL_ISO_DATETIME] NVARCHAR(256)  UNIQUE NULL,  \
+            [RUN_DESCRIPTION] NVARCHAR(256)  NULL  \
             );  \
     ''')
 
@@ -50,24 +51,27 @@ def create_tables_if_necessary(conn):
     conn.commit()
 
 
-def initialize_for_this_run(conn, project_name):
+def initialize_for_this_run(conn, project_name, run_description):
     '''
     Obtains or creates a DRC_PROJECT row and creates a new DRC_RUN row
     '''
     c = conn.cursor()
 
     # Get or create Project
-    c.execute('''SELECT * FROM DRC_PROJECT WHERE PRO_NAME = "{proj_name}"'''.format(proj_name=project_name))
+    c.execute('''SELECT * FROM DRC_PROJECT WHERE PRO_NAME = "{proj_name}"'''.
+              format(proj_name=project_name))
     id_exists = c.fetchone()
     if id_exists:
         proj_id = id_exists[0]
     else:
-        c.execute('''INSERT INTO DRC_PROJECT (PRO_NAME) VALUES ("{proj_name}")'''.format(proj_name=project_name))
+        c.execute('''INSERT INTO DRC_PROJECT (PRO_NAME) VALUES ("{proj_name}")'''.
+                  format(proj_name=project_name))
         proj_id = c.lastrowid
 
     # Create a Run row
-    comment = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-    c.execute('''INSERT INTO DRC_RUN (RUN_PRO_ID, RUN_COMMENT) VALUES ({pro_id}, "{the_comment}")'''.format(pro_id=proj_id, the_comment=comment))
+    curr_datetime = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+    c.execute('''INSERT INTO DRC_RUN (RUN_PRO_ID, RUN_LOCAL_ISO_DATETIME, RUN_DESCRIPTION) VALUES ({pro_id}, "{local_iso}", "{desc}")'''.
+              format(pro_id=proj_id, local_iso=curr_datetime, desc=run_description))
     run_id = c.lastrowid
 
     conn.commit()
@@ -82,22 +86,26 @@ def insert_run_result(conn, proj_id, run_id, tab_name, row_count):
 
     c = conn.cursor()
     # Get or create Project
-    c.execute('''SELECT * FROM DRC_MODEL WHERE MOD_PRO_ID = {proj_id} AND MOD_TABLENAME = "{table_name}"'''.format(proj_id=proj_id, table_name=tab_name))
+    c.execute('''SELECT * FROM DRC_MODEL WHERE MOD_PRO_ID = {proj_id} AND MOD_TABLENAME = "{table_name}"'''.
+              format(proj_id=proj_id, table_name=tab_name))
     id_exists = c.fetchone()
     if id_exists:
         mod_id = id_exists[0]
     else:
-        c.execute('''INSERT INTO DRC_MODEL (MOD_PRO_ID, MOD_TABLENAME) VALUES ({pro_id}, "{table_name}")'''.format(pro_id=proj_id, table_name=tab_name))
+        c.execute('''INSERT INTO DRC_MODEL (MOD_PRO_ID, MOD_TABLENAME) VALUES ({pro_id}, "{table_name}")'''.
+                  format(pro_id=proj_id, table_name=tab_name))
         mod_id = c.lastrowid
 
     # Create a Result row
     comment = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-    c.execute('''INSERT INTO DRC_RUN_RESULT (RUR_RUN_ID, RUR_MOD_ID, RUR_ROW_COUNT) VALUES ({run_id}, {mod_id}, {rowcnt})'''.format(run_id=run_id, mod_id=mod_id, rowcnt=row_count))
+    c.execute('''INSERT INTO DRC_RUN_RESULT (RUR_RUN_ID, RUR_MOD_ID,
+              RUR_ROW_COUNT) VALUES ({run_id}, {mod_id}, {rowcnt})'''.
+              format(run_id=run_id, mod_id=mod_id, rowcnt=row_count))
 
     conn.commit()
 
 
-def get_model_info(project_name):
+def get_model_info(project_name, run_description, echotostdout):
     '''
     Dump all models and their row counts to the screen
     '''
@@ -108,11 +116,8 @@ def get_model_info(project_name):
 
     with sqlite3.connect('./django-row-tracker-data/drtd.db') as conn:
         cursor = conn.cursor()
-        cursor.execute('SELECT SQLITE_VERSION()')
-        data = cursor.fetchone()
-        print 'SQLite version: ', data
         create_tables_if_necessary(conn)
-        dic_id = initialize_for_this_run(conn, project_name)
+        dic_id = initialize_for_this_run(conn, project_name, run_description)
 
         project_models = models.get_models(include_auto_created=True)
 
@@ -120,7 +125,8 @@ def get_model_info(project_name):
             try:
                 table_name = them._meta.db_table
                 row_count = them.objects.all().count()
-                print table_name + " " + str(row_count)
+                if echotostdout:
+                    print table_name + " " + str(row_count)
                 insert_run_result(conn, dic_id['proj_id'], dic_id['run_id'], table_name, row_count)
             except DatabaseError:
                 print "Unexpected error and continuing:", sys.exc_info()[0], " ", them
@@ -138,6 +144,9 @@ def process_args():
     parser.add_argument('--version', action='version', version='%(prog)s - Release : 1.1')
     parser.add_argument("path_to_project_root", help="Path to the root of the Django project of interest (that is the directory below 'manage.py')")
     parser.add_argument("project_name", help='''The name of the Django project of interest''')
+    parser.add_argument("--rundescription", default='', help='''An optional description of the state of the Django Project at the time the analysis is run''')
+    parser.add_argument('--echotostdout', dest='echotostdout', action='store_true', help='''When set the data written to the database is echoed to stdout''')
+    parser.set_defaults(echotostdout=False)
     args = parser.parse_args()
 
     return args
@@ -159,7 +168,7 @@ def main():
     '''
     args = process_args()
     setup_env(args)
-    get_model_info(args.project_name)
+    get_model_info(args.project_name, args.rundescription, args.echotostdout)
 
 
 if __name__ == "__main__":
